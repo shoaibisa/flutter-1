b50c347c579a28ff049c95fd809408a5fe69dda5
shoaibisa <shoaibisa1@gmail.com>
2022-11-22 19:46:46 +0530

    till StateLess widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,13 @@ class MahiCard extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Mahi Id'),
+        title: Text(
+          'Mahi Portfolio',
+          style: TextStyle(
+            color: Colors.amber,
+            fontFamily: 'IndieFlower'
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Colors.grey[800],
         elevation: 0.0,
@@ -20,14 +26,68 @@ class MahiCard extends StatelessWidget {
       body:Padding(
         padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/mahi.jpeg'),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 50,
+              color: Colors.grey,
+            ),
             Text(
               'NAME',
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2.0,
               ),
-            )
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              'MAHI AKHTAR',
+              style: TextStyle(
+                color: Colors.deepOrangeAccent,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text(
+              'CURRENT LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              '101',
+              style: TextStyle(
+                color: Colors.lightGreenAccent,
+                fontSize: 30.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.mail,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  'mahi@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey
+                  ),
+                ),
+              ],
+            ) ,
           ],
         ),
       ),
